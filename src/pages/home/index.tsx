9dddbf717b0d5dbfb80680cafc98adf6f7b7ed76
junyoungp <jyjyjy89@naver.com>
2024-05-19 08:53:29 +0900

    carousel 컴포넌트 작업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import Header from "../../components/Header";
 import PostList from "../../components/PostList";
 import Footer from "../../components/Footer";
+import Carousel from "../../components/Carousel";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Carousel />
       <PostList />
       <Footer />
     </>
